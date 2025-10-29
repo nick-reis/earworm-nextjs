8ad32b3c4c76d1809319164807b98f179176c79a
Nick <74227982+nick-reis@users.noreply.github.com>
2025-10-29 17:08:53 -0400

    feat: setup Earworm base project
    - Initialize Next.js + TypeScript
    - Add NextAuth for Spotify login
    - Include TailwindCSS and project metadata

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Earworm",
+  description:
+    "Earworm is an AI-powered music companion that learns your taste from Spotify and builds smarter, personalized playlists.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
