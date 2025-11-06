d8326d88a620567f89767bd3011352afd72bc5bd
Nick <74227982+nick-reis@users.noreply.github.com>
2025-11-05 19:39:37 -0500

    fix: change app icon to earworm logo
    - Update app icon to earworm logo (public/icon.svg)
    - Added earworm font Fredoka

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Fredoka } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,7 +13,15 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const fredoka = Fredoka({
+  variable: "--font-fredoka",
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
+  icons: {
+    icon: "/icon.svg",
+  },
   title: "Earworm",
   description:
     "Earworm is an AI-powered music companion that learns your taste from Spotify and builds smarter, personalized playlists.",
@@ -24,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${fredoka.variable}`}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
